6d36fdcdddfbca7362a5c3c8b7cb88b4b414d4c8
mbolger8 <mjbolger462@gmail.com>
2026-03-01 15:44:20 -0500

    Creating PerformanceDataEvaluation File

--- src/model/PerformanceDataEvaluation.java
+++ src/model/PerformanceDataEvaluation.java
@@ -2,4 +2,19 @@ package src.model;
 
 public class PerformanceDataEvaluation {
     
+    public double calculateOverallPerformance(TeamStats teamStats, PlayerStats playerStats) {
+        double teamPerformance = teamStats.getWins() / (teamStats.getWins() + teamStats.getLosses());
+        double playerPerformance = (playerStats.getGoals() + playerStats.getAssists()) / 10;
+        return (teamPerformance + playerPerformance) / 2;
+    }
+
+    public String evaluatePerformance(double overallPerformance) {
+        if (overallPerformance >= 0.8) {
+            return "Excellent";
+        } else if (overallPerformance >= 0.5) {
+            return "Good";
+        } else {
+            return "Needs Improvement";
+        }
+    }
 }

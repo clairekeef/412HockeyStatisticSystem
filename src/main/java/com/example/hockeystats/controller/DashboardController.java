70da0e9a9efcfbf29554e959e1dfcc2e46d6096a
clairekeef <jkeefclaire@gmail.com>
2026-03-16 10:05:32 -0400

    updating dashboard to load in browser

--- src/main/java/com/example/hockeystats/controller/DashboardController.java
+++ src/main/java/com/example/hockeystats/controller/DashboardController.java
@@ -1,16 +1,26 @@
 package com.example.hockeystats.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.hockeystats.view.Dashboard;
 
 @RestController
 public class DashboardController {
 
+    Dashboard dashboard = new Dashboard();
+
     @GetMapping("/dashboard")
-    public String getDashboard() {
-        return "Dashboard loaded!";
+    public Map<String,Object> getTournamentOverview() {
+
+        return dashboard.viewTournamentOverview();
+
     }
 }
+
+// go to https://localhost:8080/ to view
 
 /*
 mvn clean compile

eb2b6b01f89a75af89af2a89497198d0091aab86
marC3423 <mfr5829@psu.edu>
2026-03-17 19:55:15 -0400

    added unit tests for game data

--- src/test/java/com/example/hockeystats/GameRepoTest.java
+++ src/test/java/com/example/hockeystats/GameRepoTest.java
@@ -1,5 +1,39 @@
 package com.example.hockeystats;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.hockeystats.model.Game;
+import com.example.hockeystats.model.GameDataRepository;
+
 public class GameRepoTest {
     
+    @Test
+    void testLoadAmt(){
+       GameDataRepository repo = new GameDataRepository("games.csv") ;
+       assertEquals(20, repo.getAllGames()
+    );
+    }
+
+    @Test
+    void testGetGameByID(){
+        GameDataRepository repo = new GameDataRepository("games.csv") ;
+        Game game = repo.getGameByID("W-CAN-USA-2022");
+
+        assertNotNull(game);
+        assertEquals("Canada", game.getTeamCountry());
+        assertEquals("USA", game.getOpponentTeam());
+    }
+
+    @Test
+    void testGetGamesByTeam(){
+        GameDataRepository repo = new GameDataRepository("games.csv") ;
+        List<Game> canadaGames = repo.getGamesByTeam("canada");
+        assertEquals(4, canadaGames.size());
+    }
+
 }
